c20530183e037e1b39e582cd3d1d98137f40735d
Raphael Kaden <raphael.kaden14@gmail.com>
2024-04-26 10:03:33 +0200

    login with javascript

---
+++ webserverKoeln/login.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("login-form");
+
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault()
+
+  const formData = new FormData();
+  formData.append(
+    'username',
+    document.querySelector('input[name="username"]').value
+  )
+  formData.append(
+    'key',
+    document.querySelector('input[name="key"]').value
+  )
+
+  fetch("http:/192.168.90.10:5000/api/auth",
+  {
+    method: "POST",
+    body: formData,
+  })
+  .then(response => console.log(response))
+  .catch(error => console.log(error))
+}
--- webserverKoeln/login.php
+++ webserverKoeln/login.php
@@ -1,26 +1,27 @@
 <?php
 $url = 'http:/192.168.90.10:5000/api/auth';
 $data = $_POST;
+
 unset($data["remember"]);
+print_r($data)
+// // Prepare POST data 
+// $options = [ 
+//     'http' => [ 
+//         'method'  => 'POST', 
+//         'header'  => 'Content-type: application/x-www-form-urlencoded', 
+//         'content' => http_build_query($data), 
+//     ], 
+// ]; 
+  
+// // Create stream context 
+// $context  = stream_context_create($options); 
+  
+// // Perform POST request 
+// $response = file_get_contents($url, false, $context); 
+  
+// // Display the response 
+// echo($response); 
 
-// Prepare POST data 
-$options = [ 
-    'http' => [ 
-        'method'  => 'POST', 
-        'header'  => 'Content-type: application/x-www-form-urlencoded', 
-        'content' => http_build_query($data), 
-    ], 
-]; 
-  
-// Create stream context 
-$context  = stream_context_create($options); 
-  
-// Perform POST request 
-$response = file_get_contents($url, false, $context); 
-  
-// Display the response 
-echo($response); 
-
-print_r($response)
+// print_r($response);
   
 ?>
