ab5e54e19d502ce15e0eb8cda197899572aed8aa
Raphael Kaden <raphael.kaden14@gmail.com>
2024-04-26 09:19:20 +0200

    send data to api

--- webserverKoeln/login.php
+++ webserverKoeln/login.php
@@ -3,20 +3,22 @@ $url = 'http:/192.168.90.10:5000/api/auth';
 $data = $_POST;
 unset($data["remember"]);
 
-// use key 'http' even if you send the request to https://...
-$options = [
-    'http' => [
-        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method' => 'POST',
-        'content' => http_build_query($data),
-    ],
-];
-
-$context = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === false) {
-    /* Handle error */
-}
-
-var_dump($result);
+// Prepare POST data 
+$options = [ 
+    'http' => [ 
+        'method'  => 'POST', 
+        'header'  => 'Content-type: application/x-www-form-urlencoded', 
+        'content' => http_build_query($data), 
+    ], 
+]; 
+  
+// Create stream context 
+$context  = stream_context_create($options); 
+  
+// Perform POST request 
+$response = file_get_contents($url, false, $context); 
+  
+// Display the response 
+echo $response; 
+  
 ?>
